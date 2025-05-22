6c7f6ccfb9c8184ddd5695132477389707af171e
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-05-22 14:10:58 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/tekaxis/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/tekaxis/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -3,19 +3,23 @@ package dev.tekaxis.CadastroDeNinjas.Missoes;
 import java.util.List;
 import dev.tekaxis.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class MissaoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String descricao;
     private char dificuldade;
-
     //Varios ninjas para uma unica missão
     @OneToMany(mappedBy = "missoes")
     @JoinColumn(name = "missoes_id") // Foreign Key - Chave estrangeira
