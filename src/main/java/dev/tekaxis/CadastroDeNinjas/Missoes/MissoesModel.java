3cefcde72419d17e28f740076fab95674f57c48e
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-05-30 18:52:53 -0300

    fix: Adiciona a annotation @JsonIgnore

--- src/main/java/dev/tekaxis/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/tekaxis/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,6 +1,8 @@
 package dev.tekaxis.CadastroDeNinjas.Missoes;
 
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.tekaxis.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -23,7 +25,10 @@ public class MissoesModel {
     private String descricao;
     @Column(name = "dificuldade")
     private char dificuldade;
+
     //Varios ninjas para uma unica missão
+
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninja;
 }
