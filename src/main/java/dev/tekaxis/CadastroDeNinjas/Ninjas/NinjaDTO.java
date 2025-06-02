e5809a38ac2dfeef7ceb912c9bfd8711dcd2be0d
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-06-02 15:49:20 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x DTO

--- src/main/java/dev/tekaxis/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/tekaxis/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,19 @@
 package dev.tekaxis.CadastroDeNinjas.Ninjas;
+import dev.tekaxis.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+
+        private Long id;
+        private String nome;
+        private String email;
+        private String imgUrl;
+        private int idade;
+        private String rank;
+        private MissoesModel missoes;
 }
